884604a3f0113540b5214adcca8d233cfad01e91
Emna-Elizabeth <emnaelizabethjose12.4@gmail.com>
2024-05-14 22:31:39 +0530

    final commit

--- js/intro.js
+++ js/intro.js
@@ -1,12 +1,4 @@
-const image=document.getElementById("image");
-function changeImage(){
-    if(image.src.includes("./assets/Media (1).jpg")){
-        image.src="./assets/Media.jpg";
-    }
-    else{
-        image.src="./assets/Media (1).jpg";
-    }
-}
+
 const text="I am Emna Elizabeth Jose";
 const speed=300;
 
